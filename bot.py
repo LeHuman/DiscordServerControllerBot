4fc7ceb11eb49a47a5eb26fe86ea34dd8468c97c
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-04-22 19:03:20 -0500

    hmm

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 import time
 import urllib.request
 
@@ -25,118 +26,129 @@ cache = ""
 requestCool = 35  # Cooldown on API
 requestTime = 0
 
-
 ABCs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 
 class FONT:
-    goth = {
-        ord(s): d
-        for s, d in zip(ABCs, "𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷")
-    }
-    gothBold = {
-        ord(s): d
-        for s, d in zip(ABCs, "𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟")
-    }
-    smol = {
-        ord(s): d
-        for s, d in zip(ABCs, "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖqʳˢᵗᵘᵛʷˣʸᶻ")
-    }
-    math = {
-        ord(s): d
-        for s, d in zip(ABCs, "𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃")
-    }
+    goth = {ord(s): d for s, d in zip(ABCs, "𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷")}
+    gothBold = {ord(s): d for s, d in zip(ABCs, "𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟")}
+    smol = {ord(s): d for s, d in zip(ABCs, "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖqʳˢᵗᵘᵛʷˣʸᶻ")}
+    math = {ord(s): d for s, d in zip(ABCs, "𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃")}
+    band = {ord(s): d for s, d in zip(ABCs, "ᗩᗷᑕᗪEᖴGᕼIᒍKᒪᗰᑎOᑭᑫᖇᔕTᑌᐯᗯ᙭YᘔᗩᗷᑕᗪEᖴGᕼIᒍKᒪᗰᑎOᑭᑫᖇᔕTᑌᐯᗯ᙭Yᘔ")}
+    mono = {ord(s): d for s, d in zip(ABCs, "𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣")}
 
 
-# SPEECH
-# mumble
-# -------
-#   info
-# -------
-# foot
+SPEECHFONT = FONT.goth
 
 
-def getEmbedded(
-    speech="", mumble="‎", info="‎", foot="", smallMumble=False, smallFoot=False
+def getEmbedded(speech="", mumble="‎", info="‎", foot="", smallMumble=False, smallFoot=False, mention=None):
+    speech = str(speech).translate(SPEECHFONT)
+    if smallFoot:
+        foot = str(foot).translate(FONT.smol)
+    if str(info) != "‎":  # info has a blank unicode as it's default
+        info = "```\n" + str(info) + "\n```"
+    if str(mumble) != "‎":  # mumble has a blank unicode as it's default
+        if smallMumble:
+            mumble = str(mumble).translate(FONT.smol)
+        else:
+            mumble = str(mumble)
+
+    embed = discord.Embed(title=speech, color=BOTCOLOR, description=mention if mention else "")
+    embed.add_field(name=mumble, value=info)
+
+    if foot != "":
+        embed.set_footer(text=foot)
+    return embed
+
+
+def getEmbeddedFactory(
+    speechStr="", mumbleStr="‎", infoStr="‎", footStr="", smallMumble=False, smallFoot=False,
 ):
-    try:
-        speech = str(speech).translate(FONT.goth)
+    # What final func should request
+    rSpch = speechStr == ""
+    rMmbl = mumbleStr == "‎"
+    rInfo = infoStr == "‎"
+    rFoot = footStr == ""
+
+    if not rSpch:
+        speechStr = str(speechStr).translate(SPEECHFONT)
+
+    if not rMmbl:
+        if smallMumble:
+            mumbleStr = str(mumbleStr).translate(FONT.smol)
+        else:
+            mumbleStr = str(mumbleStr)
+
+    if not rInfo:
+        infoStr = "```\n" + str(infoStr) + "\n```"
+
+    if not rFoot:
         if smallFoot:
-            foot = str(foot).translate(FONT.smol)
-        if str(info) != "‎":  # info has a blank unicode as it's default
-            info = "```\n" + str(info) + "\n```"
-        # else:
-        #     info = mumble
-        #     mumble = "‎" # blank unicode
-        if str(mumble) != "‎":  # mumble has a blank unicode as it's default
-            if smallMumble:
-                mumble = str(mumble).translate(FONT.smol)
-            else:
-                mumble = str(mumble)
+            footStr = str(footStr).translate(FONT.smol)
+        else:
+            footStr = str(footStr)
 
-        embed = discord.Embed(title=speech, color=BOTCOLOR)  # , description=""
-        embed.add_field(name=mumble, value=info)
-
-        if foot != "":
+    def embedLoad(mention=None, speech="", mumble="‎", info="‎", foot=""):
+        embed = discord.Embed(
+            title=str(speech).translate(SPEECHFONT) if rSpch else speechStr, color=BOTCOLOR, description=mention if mention else "",
+        )
+        embed.add_field(
+            name=mumble if rMmbl and str(mumble) != "‎" else mumbleStr,
+            value="```\n" + str(info) + "\n```" if rInfo and str(info) != "‎" else infoStr,
+            inline=True,
+        )
+        if footStr != "":
+            embed.set_footer(text=footStr)
+        elif foot != "":
             embed.set_footer(text=foot)
         return embed
-    except Exception as e:
-        print(e)
+
+    return embedLoad
 
 
-def spamMsg(time):
+def spamMsg(time, mention):
     return getEmbedded(
         "Someone has already casted",
-        info="You must wait for at least "
-        + format_timespan(int(time))
-        + " before someone casts again",
+        "Use the status command to check current status",
+        "You must wait for at least " + format_timespan(int(time)) + " before someone casts again",
+        mention=mention,
+        smallMumble=True,
     )
 
 
 class BOTMSG:
     class embed:
-        caster = getEmbedded(
-            "***You are not capable of casting!***",
-            "**you must have the role of caster**",
-        )
-        serverOn = getEmbedded("Turning the server on", "This may take a few minutes")
-        serverOff = getEmbedded("Turning the server off", "This may take a few minutes")
-        lack = getEmbedded(
-            "**You lack the capability of casting this commmand**",
-            "Yuh aint allowed",
-            smallMumble=True,
-        )
-        error = getEmbedded(
+        caster = getEmbeddedFactory("***You are not capable of casting!***", "**you must have the role of caster**")
+        serverOn = getEmbeddedFactory("Turning the server on", "This may take a few minutes")
+        serverOff = getEmbeddedFactory("Turning the server off", "This may take a few minutes")
+        lack = getEmbeddedFactory("**You lack the capability of casting this commmand**", "Yuh aint allowed", smallMumble=True,)
+        error = getEmbeddedFactory(
             "**God fuckin dammit**",
             "not this shit again",
-            "There has been an issue with getting that info",
+            "There has been an issue\nPlease, report this issue\nto the issuing department\n\t\t\t\tThank you",
             "fuck",
             True,
         )
-        helper = getEmbedded(
+        helper = getEmbeddedFactory(
             "Here are the current available casts",
-            info="Turn on|off : Turn the server on or off\nSwitch on|off : Same as Turn\nStatus : Get the status of the server\nHelp: Show this message",
-            foot="not case sensitive btw",
+            infoStr="Turn on|off : Turn the server on or off\nSwitch on|off : Same as Turn\nStatus : Get the status of the server\nHelp: Show this message",
+            footStr="not case sensitive btw",
         )
-        errorHelp = getEmbedded(
+        errorHelp = getEmbeddedFactory(
             "Allow me to remind you about the available casts",
-            info="Turn on|off : Turn the server on or off\nSwitch on|off : Same as Turn\nStatus : Get the status of the server\nHelp: Show this message",
-            foot="not case sensitive btw",
+            infoStr="Turn on|off : Turn the server on or off\nSwitch on|off : Same as Turn\nStatus : Get the status of the server\nHelp: Show this message",
+            footStr="not case sensitive btw",
         )
 
     give = "Here ya go"
-    wait = "Allow me a second".translate(FONT.goth) + "..."
-    what = "What. \n".translate(FONT.goth)
-    unknown = "I don't understand".translate(FONT.goth)
-    confused = "You seem confused".translate(FONT.goth)
+    wait = "Allow me a second".translate(SPEECHFONT) + "..."
+    what = "What. \n".translate(SPEECHFONT)
+    unknown = "I don't understand".translate(SPEECHFONT)
+    confused = "You seem confused".translate(SPEECHFONT)
 
 
-def formatAPImsg(data):
-    final = (
-        data["hostname"]
-        + " is currently "
-        + ("online" if data["online"] else "offline")
-    )
+def formatAPImsg(ctx, data):
+    final = data["hostname"] + " is currently " + ("online" if data["online"] else "offline")
 
     # if "motd" in data:
     #     final += " " + data["motd"]["clean"]
@@ -148,14 +160,14 @@ def formatAPImsg(data):
         apiUpdate = curr
     lastapiUpdate = format_timespan(curr - apiUpdate + curr - requestTime)
     lastUpdate = "last update: " + lastapiUpdate + " ago"
-    return getEmbedded(BOTMSG.give, lastUpdate, final, "mcsrvstat.us")
+    return getEmbedded(BOTMSG.give, lastUpdate, final, "mcsrvstat.us", mention=ctx.message.author.mention)
 
 
 def APIWillUpdate():
     return int(time.time()) - requestTime > requestCool
 
 
-def getAltStatus(data=None):
+def getAltStatus(ctx, data=None):
     global cache, requestTime
     try:
         if not data:
@@ -166,11 +178,11 @@ def getAltStatus(data=None):
                     data = json.loads(url.read().decode())
                     cache = data
             else:
-                return getAltStatus(cache)
+                return getAltStatus(ctx, cache)
     except Exception as e:
         print(e)
-        return BOTMSG.embed.error
-    return formatAPImsg(data)
+        return BOTMSG.embed.error(ctx.message.author.mention)
+    return formatAPImsg(ctx, data)
 
 
 @bot.event
@@ -180,15 +192,7 @@ async def on_ready():
 
 @bot.command(name="help")
 async def help(ctx):
-    await ctx.send(embed=BOTMSG.embed.helper)
-
-
-@bot.command(name="clear")
-@commands.is_owner()
-async def clear(ctx):
-    await ctx.send(
-        "‎\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n‎"
-    )
+    await ctx.send(embed=BOTMSG.embed.helper(ctx.message.author.mention))
 
 
 @bot.command(name="turn")
@@ -197,16 +201,16 @@ async def clear(ctx):
 @commands.max_concurrency(1, per=BucketType.channel, wait=False)
 async def turn(ctx, state: str = None):
     if state == None:
-        await ctx.send(embed=BOTMSG.embed.errorHelp)
+        await ctx.send(embed=BOTMSG.embed.errorHelp(ctx.message.author.mention))
         return
 
     if state == "on":
-        await ctx.send(embed=BOTMSG.embed.serverOn)
+        await ctx.send(embed=BOTMSG.embed.serverOn(ctx.message.author.mention))
         return
 
     if state == "off":
         if commands.is_owner():
-            await ctx.send(embed=BOTMSG.embed.serverOff)
+            await ctx.send(embed=BOTMSG.embed.serverOff(ctx.message.author.mention))
         else:
             raise commands.NotOwner
         return
@@ -217,6 +221,18 @@ async def turn(ctx, state: str = None):
 @bot.command(name="switch", pass_context=True)
 async def switch(ctx):
     await turn.invoke(ctx)
+
+
+@bot.command(name="error")
+@commands.is_owner()
+async def error(ctx):
+    await ctx.send(embed=BOTMSG.embed.error(ctx.message.author.mention))
+
+
+@bot.command(name="clear")
+@commands.is_owner()
+async def clear(ctx):
+    await ctx.send("‎\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n‎")
 
 
 # TODO: Check if server is actually up and running then reset cooldown if so
@@ -230,8 +246,11 @@ def refreshCommands(ctx):
 async def status(ctx):
     if APIWillUpdate():
         await ctx.send(BOTMSG.wait)
-    response = getAltStatus()
+    response = getAltStatus(ctx)
     await ctx.send(embed=response)
+
+
+helpyRun = False
 
 
 @bot.event
@@ -241,7 +260,6 @@ async def on_command_error(ctx, error):
 
     print(error)
     error = getattr(error, "original", error)
-    print(error)
 
     cool = (commands.CommandOnCooldown, commands.MaxConcurrencyReached)
     role = commands.MissingRole
@@ -249,22 +267,28 @@ async def on_command_error(ctx, error):
     helpy = (commands.UserInputError, commands.CommandNotFound)
 
     if isinstance(error, helpy):
-        time.sleep(1)
-        await ctx.send(BOTMSG.what)
-        time.sleep(2)
-        await ctx.send(BOTMSG.confused)
-        time.sleep(1)
-        await ctx.send(embed=BOTMSG.embed.errorHelp)
-        refreshCommands(ctx)
+        global helpyRun
+        if not helpyRun:
+            helpyRun = True
+            if random.randint(0, 100) < 30:
+                await ctx.send(BOTMSG.what)
+                time.sleep(2)
+            if random.randint(0, 100) < 55:
+                await ctx.send(BOTMSG.confused)
+            await ctx.send(embed=BOTMSG.embed.errorHelp(ctx.message.author.mention))
+            refreshCommands(ctx)
+            helpyRun = False
+        else:
+            return
 
     if isinstance(error, cool):
-        await ctx.send(embed=spamMsg(error.retry_after))
+        await ctx.send(embed=spamMsg(error.retry_after, ctx.message.author.mention))
 
     if isinstance(error, role):
-        await ctx.send(embed=BOTMSG.embed.caster)
+        await ctx.send(embed=BOTMSG.embed.caster(ctx.message.author.mention))
 
     if isinstance(error, owner):
-        await ctx.send(embed=BOTMSG.embed.lack)
+        await ctx.send(embed=BOTMSG.embed.lack(ctx.message.author.mention))
 
     if isinstance(error, commands.BadArgument):
         await ctx.send(BOTMSG.unknown)
